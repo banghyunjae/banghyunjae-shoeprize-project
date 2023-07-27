69d3085fc2e850a33903c5e5b673266bbce08730
banghyunjae <banghyunjae@naver.com>
2023-07-28 00:43:36 +0900

    :sparkles:add: 제조사 모델과 제품 모델
    제조사
    - 제조사 명과 제조사 영문 명은 CharField를 사용하여 50글자 내로 작성
    
    제품
    - 제품명, 제품 영문 명: CharField를 사용 50글자 내
    - 제품 코드: CharField를 사용 50글자 내
    - 제조사: ForeignKey 외래키를  사용하여 참조(제조사 삭제시 제품도 삭제)
    - 출시일시: DateField를 사용
    - 가격: 달러 유로, 엔 등의 정보를 저장하려면 소수점이 들어가야해서 DecimalField를 사용, 최대자릿수와 소수자리수 지정
    - 관리자코멘트 : TextField를 사용 100자 내

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class menufact(models.Model):
+    name = models.CharField(max_length=50)
+    name_en = models.CharField(max_length=50)
+
+
+class product(models.Model):
+    name = models.CharField(max_length=50)
+    name_en = models.CharField(max_length=50)
+    product_code = models.CharField(max_length=10)
+    menufact = models.ForeignKey(menufact, on_delete=models.CASCADE)
+    release_date = models.DateField()
+    price = models.DecimalField((""), max_digits=5, decimal_places=2)
+    admin_comment = models.TextField(max_length=100)
