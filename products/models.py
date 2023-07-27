f9d95791edfc6e628a6901ef24b1008e624b6f00
banghyunjae <banghyunjae@naver.com>
2023-07-28 01:11:16 +0900

    :poop:refact: 컨벤션 실수 수정

--- products/models.py
+++ products/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 
-class menufact(models.Model):
+class Manufact(models.Model):
     name = models.CharField(max_length=50)
     name_en = models.CharField(max_length=50)
 
 
-class product(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=50)
     name_en = models.CharField(max_length=50)
     product_code = models.CharField(max_length=10)
-    menufact = models.ForeignKey(menufact, on_delete=models.CASCADE)
+    manufact = models.ForeignKey(Manufact, on_delete=models.CASCADE)
     release_date = models.DateField()
     price = models.DecimalField((""), max_digits=5, decimal_places=2)
     admin_comment = models.TextField(max_length=100)
