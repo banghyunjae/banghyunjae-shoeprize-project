525e3095e399e72a481832e7f815cebfba425858
banghyunjae <banghyunjae@naver.com>
2023-07-28 18:15:39 +0900

    :art:update: 주석 추가, 관리자코멘트 필드 수정
    관리자코멘트가 없어도 작성,조회 가능

--- products/models.py
+++ products/models.py
@@ -3,17 +3,25 @@ import uuid
 
 
 class Manufact(models.Model):
+    """
+    제조사 모델
+    """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     name_en = models.CharField(max_length=50)
 
 
 class Product(models.Model):
+    """
+    제품 모델
+    """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     name_en = models.CharField(max_length=50)
     product_code = models.CharField(max_length=10)
     manufact = models.ForeignKey(Manufact, on_delete=models.CASCADE)
     release_date = models.DateField()
-    price = models.DecimalField((""), max_digits=5, decimal_places=2)
-    admin_comment = models.TextField(max_length=100)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    admin_comment = models.TextField(max_length=100, blank=True, null=True)
