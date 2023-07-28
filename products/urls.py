1215233fead41cdb4adaf0de7451a2aedfdb1e6b
banghyunjae <banghyunjae@naver.com>
2023-07-28 11:56:09 +0900

    :sparkles:add: url 라우팅 설정

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+import views
+
+app_name = "products"
+
+urlpatterns = [
+    path("manufact/", views.maunfact_list.as_view()),
+    path("manufact/<uuid:id>/", views.maunfact_detail.as_view()),
+    path("product/", views.product_list.as_view()),
+    path("product/<uuid:id>/", views.product_detail.as_view()),
+]
