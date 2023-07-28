311b3519b5b9c27f2e2d90f156835c295c24958c
banghyunjae <banghyunjae@naver.com>
2023-07-28 12:01:23 +0900

    :sparkles:add: view 임시 설정
    rest_framework APIView를 사용하여 구현 예정

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import APIView
 
-# Create your views here.
+
+class maunfact_list(APIView):
+    pass
+
+
+class maunfact_detail(APIView):
+    pass
+
+
+class product_list(APIView):
+    pass
+
+
+class product_detail(APIView):
+    pass
