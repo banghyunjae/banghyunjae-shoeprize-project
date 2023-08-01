f22322f2549d48f37887ceab2798984035e0deda
banghyunjae <banghyunjae@naver.com>
2023-08-01 18:26:06 +0900

    :sparkles:add: CurrencySerializer 추가, ProductSerializer 필드, 주석 추가
    직렬화하여  json 형식으로 데이터 소통 가능

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Manufact, Product
+from .models import Manufact, Currency, Product
 
 
 class ManufactSerializer(serializers.ModelSerializer):
@@ -8,12 +8,25 @@ class ManufactSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "name_en"]
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    manufact = serializers.PrimaryKeyRelatedField(
-        queryset=Manufact.objects.all()
-    )  # postman에서 manufact를 id로 받아옴
+class CurrencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Currency
+        fields = ["name", "info"]
 
-    # manufact = ManufactSerializer(read_only=True)
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    manufact = ManufactSerializer() # 이렇게 설정하면 manufact의 모든 정보를 받아옴
+    currency = CurrencySerializer() # 이렇게 설정하면 currency의 모든 정보를 받아옴
+
+    PirmaryKeyRelatedField를 사용한 이유는 uuid를 받아오려면 id를 받아와야하기 때문에 사용
+    1. manufact와 currency를 id로 받아오기 위해서
+    2. manufact와 currency를 수정할 때, id만 받아오면 되기 때문
+    """
+
+    manufact = serializers.PrimaryKeyRelatedField(queryset=Manufact.objects.all())
+    currency = serializers.PrimaryKeyRelatedField(queryset=Currency.objects.all())
+
     class Meta:
         model = Product
-        exclude = ["admin_comment"]  # admin_comment를 제외
+        exclude = ["admin_comment"]  # admin_comment를 제외 일반인은 볼 수 없도록 설정
