c238d48972ce8ca151f93b539cb2868ce8cee6e3
banghyunjae <banghyunjae@naver.com>
2023-08-01 18:33:22 +0900

    :sparkles:create: 관리자 페이지 구현
    admin페이지에서도 CRUD가 손쉽게 가능할 수 있도록 구현
    
    그러나 uuid로 찾아서 구현해야되다보니 수정이 필요함

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Manufact, Currency, Product
 
-# Register your models here.
+
+"""
+관리자 페이지에서도 제조사, 가격 정보, 제품을 볼 수 있도록 설정
+수정, 삭제, 추가 가능
+"""
+
+
+class ManufactAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "name_en")
+
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ("name", "info")
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "name_en",
+        "product_code",
+        "manufact",
+        "release_date",
+        "price",
+        "currency",
+        "admin_comment",
+    )
+
+
+admin.site.register(Manufact, ManufactAdmin)
+admin.site.register(Currency, CurrencyAdmin)
+admin.site.register(Product, ProductAdmin)
