7e66cd7e6b66f748eff0e22518248ac95d837389
banghyunjae <banghyunjae@naver.com>
2023-08-01 18:28:39 +0900

    :sparkles:add: currency url라우팅 추가
    genericView를 사용해 수정,삭제도 손쉽게 가능하여 추가 구현

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,8 @@ app_name = "products"
 urlpatterns = [
     path("manufact/", views.maunfact_list.as_view()),
     path("manufact/<uuid:id>/", views.maunfact_detail.as_view()),
+    path("currency/", views.currency_list.as_view()),
+    path("currency/<int:id>/", views.currency_detail.as_view()),
     path("product/", views.product_list.as_view()),
     path("product/<uuid:id>/", views.product_detail.as_view()),
 ]
