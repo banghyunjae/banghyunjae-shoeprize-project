0f41ea58bc2505a1780c16c84b160bb7cd6cb354
banghyunjae <banghyunjae@naver.com>
2023-08-06 14:56:52 +0900

    :art:update: maun -> manu 오타수정

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "products"
 
 urlpatterns = [
-    path("manufact/", views.maunfact_list.as_view()),
-    path("manufact/<uuid:id>/", views.maunfact_detail.as_view()),
+    path("manufact/", views.manufact_list.as_view()),
+    path("manufact/<uuid:id>/", views.manufact_detail.as_view()),
     path("currency/", views.currency_list.as_view()),
     path("currency/<int:id>/", views.currency_detail.as_view()),
     path("product/", views.product_list.as_view()),
